aa10588b9e13020fe894d63c02ecc15cef989172
Nadia Bahrami <nadia.bahrami@gmail.com>
2022-07-16 14:28:14 -0700

    Adding problem set up

---
+++ 201-w1.js
@@ -0,0 +1,56 @@
+// Step 1
+/* Create a function called logPlace that will take two arguments. The first argument should be called 'marker' and 
+the second should be called 'thing'.  The reuslt of this function should be a console.log entry that is a string in
+the format of '(marker) thing'.
+
+ex: marker = '*',  thing = "potato" the output in the console should be '(*) potato'
+*/
+
+
+
+
+
+
+
+// Step 2
+/* Create a method called addItem that will take an array and an item.  The result should be the array with the 
+item added on to the end of it.  If that item is not a string you should instead return. "Item is not valid."
+
+ex: [], "potato" result = ["potato"]
+ex: ["potato"], "ice cream" = ["potato", "ice cream"]
+ex: [], 2 = "Item is not valid"
+*/
+
+
+
+
+
+// Step 3
+/* Create a function called bulkAdd that takes and array, an integer and a string.  The result should be the array with the
+string added to the end the number of times of the integer.  You should not use operators to do this. 
+ex: ["potato"], 3, "ice" = ["potato", "ice", "ice", "ice"]
+*/
+
+
+
+
+
+
+// Step 4
+/* Create a function called viewList that takes in an array of groceries and lists them in the console in order.
+
+ex: ["potato", "ice", "apple"] = 
+"(1) potato"
+"(2) ice"
+"(3) apple"
+*/
+
+
+
+
+
+//Step 5
+/* Create a method that called checkOff that takes an array and an item(string).  It should log the starting 
+ordered list.  Then remove the item from the list. Then it should log "I have bought the item!" Then display
+ the remaing list (ordered) and return the list. 
+ */
